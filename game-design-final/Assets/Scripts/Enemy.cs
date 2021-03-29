79b4577ef7e84c12200b9ce46d5efc9637cb5b84
Molly Clawson <66800843+mollyclawson@users.noreply.github.com>
2021-03-29 11:40:18 -0400

    Added health bar

---
+++ game-design-final/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+   public int damage = 1;
+   public float speed = 10f;
+   private Transform playerTrans;
+   private Vector2 target;
+   public GameObject hitEffectAnim;
+
+   //if bullet hits a collider, play explosion animation, then destroy effect and bullet
+   void OnTriggerEnter2D(Collider2D other){
+      if (other.gameObject.tag != "Enemy") {
+         if (other.gameObject.tag == "Player"){
+         GameObject healthBar = GameObject.FindWithTag("healthBar");
+         if(healthBar != null){
+            healthBar = healthBar.GetComponent();
+            healthBar.takeDamage(damage);
+            Debug.log("Collided");
+         }
+        }
+      }
+    }
+}
---
+++ game-design-final/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class HealthBar : MonoBehaviour {
+
+      public float startHealth = 100;
+      private float health;
+      //public GameObject deathEffect;
+      public Image healthBar;
+      public Color healthyColor = new Color(0.3f, 0.8f, 0.3f);
+      public Color unhealthyColor = new Color(0.8f, 0.3f, 0.3f);
+
+            //temporary time variables:
+      public float timeToDamage = 5f;
+      private float theTimer;
+      public float damageAmt = 10f;
+
+      private void Start () {
+            health = startHealth;
+            theTimer= timeToDamage;
+      }
+
+// this timer is just to test damage. Comment-out when no longer needed
+    //   void FixedUpdate () {
+    //         theTimer -= Time.deltaTime;
+    //         if (theTimer <= 0) {
+    //               
+    //               theTimer = timeToDamage;
+    //         }
+    //   }
+
+
+      public void SetColor(Color newColor){
+            healthBar.GetComponent<Image>().color = newColor;
+      }
+
+      public void TakeDamage (float amount){
+            health -= amount;
+            healthBar.fillAmount = health / startHealth;
+            //turn red at low health:
+            if (health < 0.3f){
+                  if ((health * 100f) % 3 <= 0){
+                        SetColor(Color.white);
+                        Die();
+                  }
+                  else {
+                        SetColor(unhealthyColor);
+                  }
+            }
+            else {
+                  SetColor(healthyColor);
+            }
+      }
+
+
+
+      public void Die(){
+            Debug.Log("You Died So Much");
+           // death stuff. change scene? how about a particle effect?
+            //Vector3 objPos = this.transform.position
+            //Instantiate(deathEffect, objPos, Quaternion.identity) as GameObject;
+           //SceneManager.LoadScene ("Scene_lose");
+      }
+}
