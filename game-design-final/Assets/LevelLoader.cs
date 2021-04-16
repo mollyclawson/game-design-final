603c01b1882e10fd30805709455a900eee1969d5
yw9211 <78363653+yw9211@users.noreply.github.com>
2021-04-16 19:40:38 +0800

    Load screen

---
+++ game-design-final/Assets/LevelLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelLoader : MonoBehaviour
+{
+  public GameObject loadingScreen;
+  public RectTransform loadingPlatform;
+  
+  public void LoadLevel (string sceneName)
+  {
+    StartCoroutine(LoadAsynchronously (sceneName));
+  }
+  
+  IEnumerator LoadAsynchronously (string sceneName)
+  {
+    AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+    
+    loadingScreen.SetActive(true);
+    
+    Vector2 position = loadingPlatform.anchoredPosition;
+    float positionX = position.x * 10f;
+    
+    while (!operation.isDone)
+    {
+      float progress = Mathf.Clamp01(operation.progress/.9f);
+      Debug.Log(progress);
+      position.x = positionX * 1f * 0.2f;
+      yield return null;
+    }
+  }
+}
--- game-design-final/Assets/Scripts/Menus/MainMenu.cs
+++ game-design-final/Assets/Scripts/Menus/MainMenu.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public Animator animator;
+    public GameObject loadingScreen;
+    public RectTransform loadingPlatform;
     
     public void PlayGame ()
     {
@@ -20,9 +23,23 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
     
-    private IEnumerator Wait()
+    IEnumerator Wait()
     {
       yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length+animator.GetCurrentAnimatorStateInfo(0).normalizedTime);
-      SceneManager.LoadScene("LevelTutorial");
+      
+      AsyncOperation operation = SceneManager.LoadSceneAsync("LevelTutorial");
+      
+      loadingScreen.SetActive(true);
+      
+      Vector2 position = loadingPlatform.anchoredPosition;
+      float positionX = position.x * 10f;
+      
+      while (!operation.isDone)
+      {
+        float progress = Mathf.Clamp01(operation.progress/.9f);
+        Debug.Log(progress);
+        position.x = positionX + (progress * 200f);
+        yield return null;
+      }
     }
 }
