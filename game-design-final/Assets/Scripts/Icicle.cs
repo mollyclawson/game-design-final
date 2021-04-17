6f4934f37e8fd4c51e6f5bc7e22f1d8c27bd9159
Molly Clawson <66800843+mollyclawson@users.noreply.github.com>
2021-04-17 15:58:17 -0400

    Icicles destroy on impact with ground

--- game-design-final/Assets/Scripts/Icicle.cs
+++ game-design-final/Assets/Scripts/Icicle.cs
@@ -8,7 +8,7 @@ public class Icicle : MonoBehaviour
     // A mask determining what is ground to the character
 	[SerializeField] private Transform m_GroundCheck;							
     // A position marking where to check if the player is grounded.
-    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    const float k_GroundedRadius = .8f; // Radius of the overlap circle to determine if grounded
 
     Rigidbody2D rb;
     public HealthBar healthBar;
@@ -46,33 +46,27 @@ public class Icicle : MonoBehaviour
             curSpeed = 0;
         }
         transform.position = new Vector2(transform.position.x, transform.position.y - curSpeed * Time.deltaTime);
-        if(IsGrounded()){
-            Debug.Log("I'm grounded");
-            
-        }
+
     }
 
-    // private void FixedUpdate()
-	// {
-	// 	bool wasGrounded = m_Grounded;
-	// 	m_Grounded = false;
+    private void FixedUpdate()
+	{
+		bool wasGrounded = m_Grounded;
+		m_Grounded = false;
 
-	// 	// The player is grounded if a circlecast to the groundcheck position hits anything designated as ground
-	// 	// This can be done using layers instead but Sample Assets will not overwrite your project settings.
-	// 	Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
-	// 	for (int i = 0; i < colliders.Length; i++)
-	// 	{
-	// 		if (colliders[i].gameObject != gameObject)
-	// 		{
-	// 			m_Grounded = true;
-	// 			if (!wasGrounded)
-    //     {
-    //     //   OnLandEvent.Invoke();
-    //       Debug.Log("landed");
-    //     }
-	// 		}
-	// 	}
-	// }
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(m_GroundCheck.position, k_GroundedRadius, m_WhatIsGround);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				m_Grounded = true;
+				if (!wasGrounded)
+                {   
+                Destroy(this.gameObject);
+                }
+			}
+		}
+	}
 
     void OnTriggerEnter2D(Collider2D col) 
     {
@@ -82,15 +76,14 @@ public class Icicle : MonoBehaviour
     }
 
     private bool IsGrounded() {
-        float extraHeightText = 0.1f;
-        RaycastHit2D raycastHit = Physics2D.Raycast(boxCollider2D.bounds.center, Vector2.down, boxCollider2D.bounds.extents.y + extraHeightText);
+        RaycastHit2D raycastHit = Physics2D.Raycast(boxCollider2D.bounds.center, Vector2.down, boxCollider2D.bounds.extents.y);
         Color rayColor; 
         if(raycastHit.collider != null) {
             rayColor = Color.green;
         } else {
             rayColor = Color.red;
         }
-        Debug.DrawRay(boxCollider2D.bounds.center, Vector2.down * (boxCollider2D.bounds.extents.y + extraHeightText), rayColor);
+        Debug.DrawRay(boxCollider2D.bounds.center, Vector2.down * boxCollider2D.bounds.extents.y, rayColor);
 
         return (raycastHit.collider != null);
     }
