6ce671ebaf6c8de7385542c2772117b1f7a98a71
Charles <depalman17@gmail.com>
2021-05-04 13:11:26 -0400

    added fans

---
+++ game-design-final/Assets/bigfanscript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bigfanscript : MonoBehaviour
+{
+	public float speed = 10f;
+	public PlayerMovement player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // player = GetComponent<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0f, 0f, speed * Time.deltaTime * player.timeScale());
+    }
+}
